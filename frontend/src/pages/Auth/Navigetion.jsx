b2f9b62e279cdcad6436543506889311ee682c62
I-codebyte <folorunshoopeyemi123456@gmail.com>
2025-10-08 04:01:40 +0100

    sidebar in navigation component

--- frontend/src/pages/Auth/Navigetion.jsx
+++ frontend/src/pages/Auth/Navigetion.jsx
@@ -26,7 +26,64 @@ export default function Navigation() {
 		setShowSideBar(false);
 	};
 
-	return <div>
-
-    </div>;
+	return (
+		<div
+			style={{ zIndex: 999 }}
+			className={`${
+				showSideBar ? "hidden" : "flex"
+			} xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black w:[4%] hover:w-[15%] h-[100vh] fixed`}
+			id="navigation-container"
+		>
+			<div className="flex flex-col justify-center space-y-4">
+				<Link
+					to="/"
+					className="flex items-center transition-transform transform hover:translate-x-2"
+				>
+					<AiOutlineHome
+						className="mr-2 mt-[3rem]"
+						size={26}
+					/>
+					<span className="hidden nav-item-name mt-[3rem]">
+						Home
+					</span>
+				</Link>
+				<Link
+					to="/shop"
+					className="flex items-center transition-transform transform hover:translate-x-2"
+				>
+					<AiOutlineShopping
+						className="mr-2 mt-[3rem]"
+						size={26}
+					/>
+					<span className="hidden nav-item-name mt-[3rem]">
+						Shop
+					</span>
+				</Link>
+				<Link
+					to="/cart"
+					className="flex items-center transition-transform transform hover:translate-x-2"
+				>
+					<AiOutlineShoppingCart
+						className="mr-2 mt-[3rem]"
+						size={26}
+					/>
+					<span className="hidden nav-item-name mt-[3rem]">
+						Cart
+					</span>
+				</Link>
+				<Link
+					to="/favourite"
+					className="flex items-center transition-transform transform hover:translate-x-2"
+				>
+					<FaHeart
+						className="mr-2 mt-[3rem]"
+						size={26}
+					/>
+					<span className="hidden nav-item-name mt-[3rem]">
+						Favourite
+					</span>
+				</Link>
+			</div>
+		</div>
+	);
 }
